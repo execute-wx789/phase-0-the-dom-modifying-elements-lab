299fd640a056569fd4aa075a30c2a7627375a32a
Yurick <YJakse789@gmail.com>
2023-07-20 11:55:10 -0400

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+// Write your code here!//
+const main = document.getElementsByTagName("main")[0];
+const divNew = document.createElement("div");
+const newHeader = document.createElement("h1");
+
+
+main.remove()
+newHeader.setAttribute("id","victory");
+newHeader.textContent = "wx789 is the champion!";
+newHeader.append(divNew);
